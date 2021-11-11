ab604c79f35ddaddf69e70bb982ea692787c71eb
ATANU JANA <65564188+atanu20@users.noreply.github.com>
2021-11-12 04:15:45 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
         <>
         <div className="p-3 bg-dark">
             <p className="text-center text-white">© 2021 nextpoll.com</p>
-
+<Link href="/postpoll' style={{color:'#AO5'}}>Post Poll</>
         </div>
 
 
